375eb174b53348a0718d7a842e1452384622432b
IkaFebrina <32430894+IkaFebrina@users.noreply.github.com>
2018-01-29 10:52:19 +0800

    class

--- class.cpp
+++ class.cpp
@@ -2,4 +2,12 @@
 #include <string>
 using namespace std;
 
+class JamTangan {
+    public:
+        void JenisJamTangan(string JamTangan);
+        string MerkJamTangan;
+        void HargaJamTangan(string harga);
+    	void JumlahJamTangan (int jumlah);
+                
+        };
 }
