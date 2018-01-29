618264400033ddf858ccfc790f9af083ab0274d3
IkaFebrina <32430894+IkaFebrina@users.noreply.github.com>
2018-01-29 10:53:34 +0800

    isi

--- class.cpp
+++ class.cpp
@@ -10,4 +10,40 @@ class JamTangan {
     	void JumlahJamTangan (int jumlah);
                 
         };
+int main(){
+    JamTangan digital,analog,chronograph,quartz; 
+
+	digital.MerkJamTangan			="merk G-Shock ";
+	analog.MerkJamTangan			="merk Swiss Army ";
+	chronograph.MerkJamTangan		="merk Alexandre Christie  ";
+	quartz.MerkJamTangan			="merk Seiko ";
+	
+	cout << "Jam Tangan Digital: \n";
+	cout << (digital.MerkJamTangan);
+	digital.HargaJamTangan("200000");
+	digital.JumlahJamTangan(1);
+	cout <<"===================================\n";
+	
+	cout << "Jam Tangan Analog: \n";
+	cout << (analog.MerkJamTangan);
+	analog.HargaJamTangan("300000");
+	analog.JumlahJamTangan(1);
+	cout <<"===================================\n";
+	
+	cout << "Jam Tangan Chronograph: \n";
+	cout << (chronograph.MerkJamTangan);
+	chronograph.HargaJamTangan("400000");
+	chronograph.JumlahJamTangan(1);
+	cout <<"===================================\n";
+	
+	cout << "Jam Tangan Quartz: \n";
+	cout << (quartz.MerkJamTangan);
+	quartz.HargaJamTangan("500000");
+	quartz.JumlahJamTangan(1);
+	cout <<"===================================\n";
+	
+
+	
+	}
+
 }
